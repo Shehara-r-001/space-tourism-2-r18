59e660a8a67e71ed05c5cf18c52fd3b4afeea28a
Shehara_r <ranathisarashehara01@gmail.com>
2022-05-10 17:32:19 +0530

    home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import logo from '../assets/shared/logo.svg';
 const Header = () => {
   return (
     <>
-      <header>
+      <header className='absolute top-0 left-0'>
         <Link to='/'>
           <img src={logo} className='' alt='logo' />
         </Link>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,27 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <h1>
-          So you want to travel to <span>space</span>
-        </h1>
-        <p>
-          Let's face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well, sit
-          back, and relax because we'll give you a truly out of this world
-          experience!
-        </p>
-        <button>
-          <Link to='/destinaton'>Explore</Link>
-        </button>
+    <div className='bg-[url("https://github.com/Shehara-r-001/space-tourism-2-r18/blob/main/src/assets/home/background-home-mobile.jpg?raw=true")] md:bg-[url("https://github.com/Shehara-r-001/space-tourism-2-r18/blob/main/src/assets/home/background-home-desktop.jpg?raw=true")] sm:bg-[url("https://github.com/Shehara-r-001/space-tourism-2-r18/blob/main/src/assets/home/background-home-tablet.jpg?raw=true")] h-screen bg-cover bg-no-repeat w-full bg-center'>
+      <div className='flex h-screen flex-col items-center justify-center lg:justify-start px-10 lg:flex-row transition-all duration-300'>
+        <div className='lg:max-w-[40%] lg:ml-60 lg:mt-10'>
+          <h1 className='flex flex-col items-center uppercase text-xl text-gray-300 lg:items-start'>
+            So you want to travel to
+            <span className='text-[40px] sm:text-[60px] uppercase lg:my-10 my-6 text-white sm:my-8'>
+              space
+            </span>
+          </h1>
+          <p className='text-center text-gray-300 lg:text-justify lg:max-w-[300px] sm:max-w-[400px] '>
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well, sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div>
+          <button className='h-40 w-40 lg:h-56 lg:w-56 lg:mt-20 text-xl text-gray-600 bg-white rounded-full uppercase mt-10 animate-pulse font-semibold lg:ml-[180px] lg:text-2xl'>
+            <Link to='/destinaton'>Explore</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
