6ef043e98cd3943b4162d4cec4efc8a872825c01
Shehara_r <ranathisarashehara01@gmail.com>
2022-05-13 11:52:28 +0530

    crew page

--- src/pages/Crew.js
+++ src/pages/Crew.js
@@ -14,14 +14,14 @@ const Crew = () => {
   const { name, images, role, bio } = dataCrew[item];
   return (
     <div className='crew-bg h-screen w-full object-cover'>
-      <div className='absolute top-[65px] ml-5 text-center lg:text-3xl text-xl uppercase font-medium'>
-        <span className='mr-2 text-gray-500 text-2xl lg:text-5xl font-semibold'>
+      <div className='absolute top-[70px] left-[30vw] lg:left-0 lg:top-[24vh] text-center lg:text-3xl text-xl uppercase font-medium w-[40vw] lg:ml-[20vw]'>
+        <span className='mr-2 text-gray-500 lg:text-5xl font-semibold text-4xl'>
           02
         </span>
         Meet your crew
       </div>
-      <div className='absolute top-[80px] w-full'>
-        <div className=''>
+      <div className='absolute top-[70px] mt-8 w-full lg:justify-between lg:w-[80vw] lg:top-[30vh] sm:flex sm:flex-col-reverse lg:flex-row-reverse'>
+        <div className='mt-4 lg:absolute lg:-right-[120px] lg:-bottom-[100px]'>
           <img
             src={
               item === 0
@@ -35,11 +35,11 @@ const Crew = () => {
                 : ''
             }
             alt={name}
-            className='h-[240px] object-cover mx-auto '
+            className='h-[240px] object-cover mx-auto lg:h-[400px]'
           />
         </div>
-        <div className='w-full'>
-          <div className='flex w-[100px] justify-between mx-auto mt-5'>
+        <div className='w-full lg:flex lg:flex-col-reverse'>
+          <div className='flex w-[100px] lg:w-[160px] justify-between mx-auto mt-5 lg:mt-10'>
             {dataCrew.map((person, index) => (
               <button key={index} onClick={() => setItem(index)} className=''>
                 {index === item ? (
@@ -51,9 +51,15 @@ const Crew = () => {
             ))}
           </div>
           <div className='w-full text-center mt-6'>
-            <h1 className='text-[18px] text-gray-400 uppercase'>{role}</h1>
-            <h1 className='text-2xl uppercase my-2'>{name}</h1>
-            <div className='w-[300px] mx-auto text-sm mt-3'>{bio}</div>
+            <h1 className='text-[18px] sm:text-[20px] lg:text-2xl text-gray-400 uppercase'>
+              {role}
+            </h1>
+            <h1 className='text-2xl sm:text-3xl lg:text-4xl lg:mt-4 uppercase my-2'>
+              {name}
+            </h1>
+            <div className='w-[300px] lg:w-[360px] lg:mt-6 lg-text-[16px] mx-auto text-sm mt-3 lg:text-justify'>
+              {bio}
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import vehicleP from '../assets/technology/image-launch-vehicle-portrait.jpg';
+import capsuleP from '../assets/technology/image-space-capsule-portrait.jpg';
+import spaceportP from '../assets/technology/image-spaceport-portrait.jpg';
+import vehicleL from '../assets/technology/image-launch-vehicle-landscape.jpg';
+import capsuleL from '../assets/technology/image-space-capsule-landscape.jpg';
+import spaceportL from '../assets/technology/image-spaceport-landscape.jpg';
+import { VscCircleLargeOutline, VscCircleLargeFilled } from 'react-icons/vsc';
+
+const data = require('../data.json');
 
 const Technology = () => {
-  return <div>Technology</div>;
+  const [dataC] = useState(data.technology);
+  const [item, setItem] = useState(0);
+
+  const { name, images, description } = dataC[item];
+
+  return (
+    <div className='tech-bg h-screen w-full'>
+      <div>
+        <div>
+          <p>
+            <span>03</span>Space launch 101
+          </p>
+        </div>
+        <div>
+          <div>
+            <img
+              src={
+                item === 0
+                  ? vehicleP
+                  : item === 1
+                  ? spaceportP
+                  : item === 2
+                  ? capsuleP
+                  : ''
+              }
+              alt={name}
+              className='h-[300px]'
+            />
+          </div>
+          <div>
+            {dataC.map((tech, index) => (
+              <button onClick={() => setItem(index)}>
+                <VscCircleLargeOutline />
+              </button>
+            ))}
+          </div>
+          <div>
+            <div>
+              <p>The technology</p>
+              <p>{name}</p>
+            </div>
+            <div>
+              <p>{description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Technology;
